644110e7a6e40a889721fbc59f3b0d8226285075
jshristi22 <jshristi17@gmail.com>
2024-09-03 14:49:43 +0530

    Completes UI and api integration for both views

--- src/presentation/components/custom_card/custom_card.tsx
+++ src/presentation/components/custom_card/custom_card.tsx
@@ -1,8 +1,8 @@
 import { IProductDataModel } from "../../../data/model/product_data.model";
 import ImageNotSupportedIcon from "@mui/icons-material/ImageNotSupported";
 import styles from "./custom_card.module.scss";
-import { Card, CardContent } from "@mui/material";
-
+import { Card, CardContent, Rating } from "@mui/material";
+import StarIcon from "@mui/icons-material/Star";
 function CustomCard({
   data,
   isDetailView = false,
@@ -11,14 +11,36 @@ function CustomCard({
   isDetailView?: boolean;
 }) {
   return (
-    <Card>
+    <Card
+      sx={{
+        width: "100%",
+      }}
+    >
       <CardContent>
-        <div className={styles.customCardContainer}>
-          <div className={`${styles.productImage} ${isDetailView ? "" : ""}`}>
+        <div
+          className={`${
+            isDetailView
+              ? styles.customCardDetailsContainer
+              : styles.customCardContainer
+          }`}
+        >
+          <div className={styles.productImage}>
             {data?.image ? (
               <img src={data?.image} alt="product image" />
             ) : (
               <ImageNotSupportedIcon />
+            )}
+            {!isDetailView && (
+              <div className={styles.rating}>
+                <StarIcon
+                  fontSize="small"
+                  sx={{
+                    color: "#faaf00",
+                  }}
+                />
+                <h6>{data?.rating?.rate}</h6>
+                <h6>({data?.rating?.count})</h6>
+              </div>
             )}
           </div>
           <div className={styles.detailsContainer}>
@@ -29,7 +51,14 @@ function CustomCard({
                 <h3>{data?.description}</h3>
               </div>
             </div>
-            <h4>{data?.price}</h4>
+            {isDetailView && (
+              <div className={styles.rating}>
+                <Rating name="read-only" value={data?.rating?.rate} readOnly />
+                <h6>{data?.rating?.rate}</h6>
+                <p> {data?.rating?.count} reviews </p>
+              </div>
+            )}
+            <h4>$ {data?.price}</h4>
           </div>
         </div>
       </CardContent>
